ee80550825d257577b31ab85ce1eea66ef707d43
Halid13 <mohamed.onifade@epitech.eu>
2023-12-05 10:49:31 +0100

    add welcome page

--- my_area/src/components/Accueil.vue
+++ my_area/src/components/Accueil.vue
@@ -4,13 +4,29 @@
       <div class="th">
         <router-link to="/login" class="text">Get Started</router-link>
       </div>
+    </div>
     <div class="lo">
       <router-link to="/login" class="text">Log in</router-link>
     </div>
     <div class="dev">
       <router-link to="/login" class="text">Developers</router-link>
     </div>
-    <img src="../assets/back.png" class="image-redimensionnee">
+    <div class="image-pos">
+      <img src="../assets/back.png" class="image-redimensionnee">
+    </div>
+    <div class="prinp-pos">
+      <p class="text-prinp">WELCOME TO</p>
+    </div>
+    <div class="prinp-pos2">
+      <p class="text-prinp">AREA</p>
+    </div>
+    <div class="sous-pos">
+      <p class="sous-text">Get a multitude of services</p>
+    </div>
+    <div class="rectangle1">
+      <div class="the">
+        <router-link to="/login" class="fina-text">Start</router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -24,7 +40,6 @@ export default {
 } 
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .rectangle {
   width: 100px; /* Largeur du rectangle */
@@ -42,17 +57,55 @@ export default {
   text-decoration: none;
 }
 .lo {
-  margin-top: -20px;
-  margin-left: -180px;
+  margin-top: -25px;
+  margin-left: 1230px;
 }
 .dev {
   margin-top: -20px;
-  margin-left: -350px;
+  margin-left: 1050px;
 }
 .image-redimensionnee {
   width: 1250px; /* Largeur souhaitée */
-  height: 550px; /* Hauteur ajustée proportionnellement */
-  margin-top: 33px;
-  margin-left: -1390px;
+  height: 600px; /* Hauteur ajustée proportionnellement */
+}
+.image-pos {
+  margin-top: 100px;
+  margin-left: 750px;
+}
+.text-prinp {
+  color: #000000;
+  font-weight: bolder;
+  font-size: 70px;
+}
+.prinp-pos {
+  margin-top: -500px;
+}
+.prinp-pos2 {
+  margin-top: -80px;
+}
+.sous-text {
+  color: #000000;
+  font-weight:lighter;
+  font-size: 30px;
+}
+.sous-pos {
+  margin-top: -50px;
+}
+.rectangle1 {
+  width: 100px; /* Largeur du rectangle */
+  height: 40px; /* Hauteur du rectangle */
+  border-radius: 30px;
+  background-color: #709CA7; /* Couleur de fond du rectangle */
+  margin-left: 730px;
+  margin-top: -10px;
+}
+.the {
+  padding-top: 7px;
+}
+.fina-text {
+  color: #000000;
+  font-weight:lighter;
+  font-size: 30px;
+  text-decoration: none;
 }
 </style>
